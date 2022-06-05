fd5a4203a2cda96661a65e3f56869d50c10f8537
jamie0708 <juniorschool20@gmail.com>
2022-06-05 14:55:53 +0500

    gg

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 7777;
 const app = express()
 
 app.use(express.json());
-// app.use(cors());
-// app.use(express.static('public'));
+app.use(cors());
+app.use(express.static('public'));
 
 app.use(usersRouter)
 
